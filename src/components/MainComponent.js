457112c421dc9ddf9c64e9afe21837516b89bad1
Chandni Soni <soni.chandni.415@gmail.com>
2022-06-23 00:22:58 +0530

    Add Header and footer. Add font awesome and bootstrap social packages.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Dishdetail from "./DishdetailComponent";
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -23,15 +24,12 @@ class Main extends Component {
     render () {
         return (
             <div>
-                <Navbar dark color='primary'>
-                <div className='container'>
-                    <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-                </div>
-                </Navbar>
+                <Header />
                 <Menu dishes={this.state.dishes}
                         onClick={(dishId) => this.onDishSelect(dishId)}/>
                 <Dishdetail 
                         dishSel={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer />
             </div>
         );
     }
