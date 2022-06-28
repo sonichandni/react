2c13ed21bf5ea36cd6cc5dbda9685e03dade92bc
Chandni Soni <soni.chandni.415@gmail.com>
2022-06-28 22:30:36 +0530

    Combining reducers.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { legacy_createStore as createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { combineReducers, legacy_createStore as createStore } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
